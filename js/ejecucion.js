118589559a65cf509d479e53d33d37b4fd08ed02
jesica <jesicaguerrerof@gmail.com>
2022-12-21 20:18:56 -0300

    proyecto subido

---
+++ js/ejecucion.js
@@ -0,0 +1,11 @@
+alert("Bienvenidxs!!");
+function inclusive()
+    {  
+        document.body.style.backgroundColor= "black";
+       
+        document.body.style.color="white";
+       
+        
+       
+        
+    }
